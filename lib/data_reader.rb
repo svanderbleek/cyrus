8d7e98783c085a1220a86856068e35271502e953
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2011-10-18 22:42:16 -0400

    DataReader spec passes

--- lib/data_reader.rb
+++ lib/data_reader.rb
@@ -32,14 +32,17 @@ class DataReader
     end
 
     def read_pipe fields
-      drop_middle_initial fields
+      raise 'invalid data' if fields.size != 6
+      order(drop_middle_initial fields)
     end
 
     def read_comma fields
-      fields
+      raise 'invalid data' if fields.size != 5
+      order fields
     end
 
     def read_space fields
+      raise 'invalid data' if fields.size != 6
       order(drop_middle_initial fields)
     end
 
@@ -48,9 +51,9 @@ class DataReader
       [last, first, *rest]
     end
 
-    def order space_fields
-      *rest, date, color = space_fields
-      [*rest, color, date]
+    def order fields
+      *rest, color, date = fields
+      [*rest, date, color]
     end
 
     def find_delimiter line
--- test/data_reader_test.rb
+++ test/data_reader_test.rb
@@ -7,21 +7,43 @@ class TestDataReader < Test::Unit::TestCase
     @reader = DataReader.new
   end
 
+  def test_exception_for_bad_data
+    bad = File.open 'test/fixtures/bad'
+    assert_raise(RuntimeError) do 
+      @reader.read bad 
+    end
+  end
+
   def test_reads_comma
     comma = File.open 'test/fixtures/comma'
     data = @reader.read comma 
+    assert_equal(3, data.size)
+    assert_equal(5, data.first.size)
+    assert_equal('CLastName', data.first.first)
   end
 
   def test_reads_space
     space = File.open 'test/fixtures/space'
     data = @reader.read space 
+    assert_equal(2, data.size)
+    assert_equal(5, data.first.size)
+    assert_equal('NLastName', data.first.first)
   end
 
   def test_reads_pipe
     pipe  = File.open 'test/fixtures/pipe'
     data = @reader.read pipe 
     assert_equal(2, data.size)
-    assert_equal('BLastName', data.first.last_name)
+    assert_equal(5, data.first.size)
+    assert_equal('BLastName', data.first.first)
   end
 
+  def test_reads_files
+    pipe  = File.open 'test/fixtures/pipe'
+    space = File.open 'test/fixtures/space'
+    comma = File.open 'test/fixtures/comma'
+
+    data = @reader.read pipe, space, comma
+    assert_equal(7, data.size)
+  end
 end
